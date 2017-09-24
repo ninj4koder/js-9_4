e656211d39f42db67267bb09a0f70faccafcfd8f
ninja_koder <grinders@tlen.pl>
2017-09-24 23:15:55 +0200

    popełnione

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function drawTree(rows) {
+  for (var i = 0; i < rows; i++) {
+    var stars = '';
+    for (var j = 0; j <= i; j++) {
+    	stars += '*';
+    }
+    console.log(stars);
+  }
+}
+
+var cycles = prompt('Ile rzędów choinki mam zmalować?');
+drawTree(cycles);
